a5ec5d0e6560cbbd24ec89c5b59f6ab61c0b5a58
windweller <leo.niecn@gmail.com>
2017-10-29 17:56:51 -0700

    solve the intermediate files location problem

--- preprocessing/producer.py
+++ preprocessing/producer.py
@@ -1,8 +1,9 @@
 """
 (might change name later)
 Based on commands, select a group of markers,
-filter based on length, ratio, cap (if needed), etc.
-merge them into one set, train/val/test split, np.shuffle (fix random seed)
+Add additional preprocessing steps for (s1, s2) here, such as delete punctuations
+Filter based on length, ratio, cap (if needed), etc.
+Merge them into one set, train/val/test split, np.shuffle (fix random seed)
 
 (then Torchtext can take it from there!)
 """
