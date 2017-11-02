ec02ae919fa07c579ad00e9f37d0e1e05ba2fd4c
Erin Bennett <erindb@stanford.edu>
2017-11-02 11:26:09 -0700

    transfer some functions over from old version

--- preprocessing/filter.py
+++ preprocessing/filter.py
@@ -6,3 +6,73 @@ and save them as intermediate files
 shuffle within each discourse marker
 """
 
+def collect_raw_sentences(source_dir, dataset, caching):
+    markers_dir = pjoin(source_dir, "markers_" + DISCOURSE_MARKER_SET_TAG)
+    output_dir = pjoin(markers_dir, "files")
+
+    if not os.path.exists(markers_dir):
+        os.makedirs(markers_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if dataset == "wikitext-103":
+        filenames = [
+            "wiki.train.tokens",
+            "wiki.valid.tokens", 
+            "wiki.test.tokens"
+        ]
+    else:
+        raise Exception("not implemented")
+
+    sentences = {marker: {"sentence": [], "previous": []} for marker in DISCOURSE_MARKERS}
+    
+    for filename in filenames:
+        print("reading {}".format(filename))
+        file_path = pjoin(source_dir, "orig", filename)
+        with io.open(file_path, 'rU', encoding="utf-8") as f:
+            # tokenize sentences
+            sentences_cache_file = file_path + ".CACHE_SENTS"
+            if caching and os.path.isfile(sentences_cache_file):
+                sent_list = pickle.load(open(sentences_cache_file, "rb"))
+            else:
+                tokens = f.read().replace("\n", ". ")
+                print("tokenizing")
+                sent_list = nltk.sent_tokenize(tokens)
+                if caching:
+                    pickle.dump(sent_list, open(sentences_cache_file, "wb"))
+
+        # check each sentence for discourse markers
+        previous_sentence = ""
+        for sentence in sent_list:
+            words = rephrase(sentence).split()  # replace "for example"
+            for marker in DISCOURSE_MARKERS:
+                if marker == "for example":
+                    proxy_marker = "for_example" 
+                else:
+                    proxy_marker = marker
+
+                if proxy_marker in [w.lower() for w in words]:
+                    sentences[marker]["sentence"].append(sentence)
+                    sentences[marker]["previous"].append(previous_sentence)
+            previous_sentence = sentence
+
+    print('writing files')
+    statistics_lines = []
+    for marker in sentences:
+        sentence_path = pjoin(output_dir, "{}_s.txt".format(marker))
+        previous_path = pjoin(output_dir, "{}_prev.txt".format(marker))
+        n_sentences = len(sentences[marker]["sentence"])
+        statistics_lines.append("{}\t{}".format(marker, n_sentences))
+        with open(sentence_path, "w") as sentence_file:
+            for s in sentences[marker]["sentence"]:
+                sentence_file.write(s + "\n")
+        with open(previous_path, "w") as previous_file:
+            for s in sentences[marker]["previous"]:
+                previous_file.write(s + "\n")
+
+    statistics_report = "\n".join(statistics_lines)
+    open(pjoin(markers_dir, "VERSION.txt"), "w").write(
+        "commit: \n\ncommand: \n\nmarkers:\n" + statistics_report
+    )
+
+
--- preprocessing/producer.py
+++ preprocessing/producer.py
@@ -7,3 +7,85 @@ merge them into one set, train/val/test split, np.shuffle (fix random seed)
 (then Torchtext can take it from there!)
 """
 
+
+def filtering(source_dir, args):
+
+    args.min_ratio = 1/args.max_ratio
+
+    marker_dir = pjoin(source_dir, "markers_" + DISCOURSE_MARKER_SET_TAG)
+    split_dir = pjoin(marker_dir, "split_train{}".format(args.train_size))
+    ssplit_dir = pjoin(split_dir, "ssplit_" + args.method)
+    input_dir = pjoin(ssplit_dir, "files")
+
+    filter_dir = pjoin(ssplit_dir, "filter_max{}_min{}_ratio{}_undersamp{}".format(
+        args.max_seq_len,
+        args.min_seq_len,
+        args.max_ratio,
+        args.undersamp_cutoff
+    ))
+    output_dir = pjoin(filter_dir, "files")
+
+    if not os.path.exists(filter_dir):
+        os.makedirs(filter_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    def get_data(element_type, split):
+        filename = "{}_{}_{}.txt".format(args.method, split, element_type)
+        file_path = pjoin(input_dir, filename)
+        return open(file_path, "rU").readlines()
+
+    frequencies = {}
+    for split in ["train", "valid", "test"]:
+        frequencies[split] = {}
+        for marker in DISCOURSE_MARKERS:
+            frequencies[split][marker] = 0
+
+    statistics_lines = []
+    for split in ["train", "valid", "test"]:
+        keep = {"s1": [], "s2": [], "label": []}
+
+        # length-based filtering
+        s1s = get_data("s1", split)
+        s2s = get_data("s2", split)
+        labels = get_data("label", split)
+        assert(len(s1s) == len(s2s) and len(s2s) == len(labels))
+        for i in range(len(s1s)):
+            s1 = s1s[i][:-1]
+            s2 = s2s[i][:-1]
+            label = labels[i][:-1]
+            len1 = len(s1.split())
+            len2 = len(s2.split())
+            ratio = float(len2)/len1
+            if args.min_seq_len<len1 and len1<args.max_seq_len \
+                    and args.min_seq_len<len2 and len2<args.max_seq_len \
+                    and args.min_ratio<ratio and ratio<args.max_ratio:
+                keep["s1"].append(s1)
+                keep["s2"].append(s2)
+                keep["label"].append(label)
+                frequencies[split][label] += 1
+
+        # write new filtered files
+        for element_type in ["s1", "s2", "label"]:
+            filename = "{}_{}_{}_{}_{}_{}.txt".format(
+                split, 
+                element_type,
+                args.method, 
+                args.max_seq_len, 
+                args.min_seq_len, 
+                args.max_ratio
+            )
+            file_path = pjoin(output_dir, filename)
+            with open(file_path, "w") as write_file:
+                for element in keep[element_type]:
+                    write_file.write(element + "\n")
+
+    statistics_lines = []
+    for split in frequencies:
+        for marker in frequencies[split]:
+            freq = frequencies[split][marker]
+            statistics_lines.append("{}\t{}\t{}".format(split, marker, freq))
+    statistics_report = "\n".join(statistics_lines)
+    open(pjoin(filter_dir, "VERSION.txt"), "w").write(
+        "commit: \n\ncommand: \n\statistics:\n" + statistics_report
+    )
