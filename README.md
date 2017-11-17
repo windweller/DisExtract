1aff4c79b58369e0259aaba91406be8832df7b88
windweller <leo.niecn@gmail.com>
2017-11-17 12:53:51 -0800

    modularize readme and preprocessing

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 
+## Dependency Pattern Instructions
+
+```python
+depdency_patterns = [
+    "still": {
+    "POS": ["RB"],
+    "S2": ["advmod"],
+    "S1": ["parataxis", "dep"],
+    "acceptable_order": "S1 S2"},
+    
+    "for example": {
+    "POS": ["NN"],
+    "S2": ["nmod"],
+    "S1": ["parataxis"],
+    "head": "example"},
+    
+    "but": {
+    "POS": ["CC"],
+    "S2": ["cc"],
+    "S1": ["conj"],
+    "flip": True}
+]
+  
+```
+
+`Acceptable_order: "S1 S2""`: a flag to reject weird discourse term like "then" referring to previous sentence, 
+but in itself a S2 S1 situation. In general it's hard to generate a valid (S1, S2) pair.
+
+`flip: True`: a flag that means rather than S2 connecting to marker, S1 to S2; it's S1 connecting to marker, 
+then S1 connects to S2.
+
+`head: "example""`: for two words, which one is the one we use to match dependency patterns. 
 
 ## TODO
 
 ☐ switch to better (more automatic) dependency parsing with corenlp
 ☐ refactor testing to be prettier
 ☐ finish making test cases and editing parser for all discourse markers
+
---
+++ preprocessing/dep_patterns.py
@@ -0,0 +1,131 @@
+# grab test cases
+ch_dependency_patterns = {
+  "然后": [{ # then
+    "POS": ["AD"],
+    "S1": ["conj"],  # the dep tag S1 -> S2
+    "S2": ["advmod"]  # the dep tag S2 -> marker
+  },
+  { # then
+    "POS": ["AD"],
+    "S1": ["dep"],  # the dep tag S1 -> S2
+    "S2": ["advmod"]  # the dep tag S2 -> marker
+  }
+  ],
+  "但是": { # but
+    "POS": ["AD"],
+    "S1": ["conj"],
+    "S2": ["advmod"]
+  },
+  "也": { # also
+    "POS": ["AD"],
+    "S1": ["conj"],
+    "S2": ["advmod"]
+  }
+}
+
+en_dependency_patterns = {
+  "after": {
+    "POS": ["IN"],
+    "S2": ["mark"], # S2 head (full S head) ---> connective
+    "S1": ["advcl", "acl"]
+  },
+  "also": {
+    "POS": ["RB"],
+    "S2": ["advmod"],
+    "S1": ["advcl"]
+  },
+  "although": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "and": {
+    "POS": ["CC"],
+    "S2": ["cc"],
+    "S1": ["conj"],
+    "flip": True
+  },
+  "as": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "before": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "but": {
+    "POS": ["CC"],
+    "S2": ["cc"],
+    "S1": ["conj"],
+    "flip": True
+  },
+  # "so": {
+  #   "POS": "IN",
+  #   "S2": "dep",
+  #   "S1": ["parataxis"],
+  #   "flip": True
+  # },
+  "so": {
+    "POS": ["IN", "RB"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "still": {
+    "POS": ["RB"],
+    "S2": ["advmod"],
+    "S1": ["parataxis", "dep"],
+    "acceptable_order": "S1 S2"
+  },
+  "though": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "because": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "however": {
+    "POS": ["RB"],
+    "S2": ["advmod"],
+    "S1": [
+        "parataxis",
+        # "ccomp" ## rejecting in favor of high precision
+    ]
+  },
+  "if": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "meanwhile": {
+    "POS": ["RB"],
+    "S2": ["advmod"],
+    "S1": ["parataxis"]
+  },
+  "while": {
+    "POS": ["IN"],
+    "S2": ["mark"],
+    "S1": ["advcl"]
+  },
+  "for example": {
+    "POS": ["NN"],
+    "S2": ["nmod"],
+    "S1": ["parataxis"],
+    "head": "example"
+  },
+  "then": {
+    "POS": ["RB"],
+    "S2": ["advmod"],
+    "S1": ["parataxis"],
+    "acceptable_order": "S1 S2"
+  },
+  "when": {
+    "POS": ["WRB"],
+    "S2": ["advmod"],
+    "S1": ["advcl"]
+  }
+}
