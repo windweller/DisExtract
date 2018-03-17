795955a23952aed2fd3f2e46ef1b10ac7237faa5
windweller <leo.niecn@gmail.com>
2018-03-17 13:34:51 -0700

    add wikitext download

--- preprocessing/corpus/wikitext/download.sh
+++ preprocessing/corpus/wikitext/download.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/pytorch/examples/master/word_language_model/data/wikitext-2/test.txt
+wget https://raw.githubusercontent.com/pytorch/examples/master/word_language_model/data/wikitext-2/valid.txt
+wget https://raw.githubusercontent.com/pytorch/examples/master/word_language_model/data/wikitext-2/train.txt
