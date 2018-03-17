89b727212bd12353d65e425596d1181150726fef
windweller <leo.niecn@gmail.com>
2018-03-17 12:14:27 -0700

    update ptb download

---
+++ preprocessing/corpus/ptb/download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://github.com/wojzaremba/lstm/tree/master/data/ptb.train.txt
+wget https://github.com/wojzaremba/lstm/tree/master/data/ptb.valid.txt
+wget https://github.com/wojzaremba/lstm/tree/master/data/ptb.test.txt
