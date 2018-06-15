f970410853b621d6f68040211ffab6872f101e93
Erin Bennett <erindb@stanford.edu>
2018-06-14 18:14:00 -0700

    add arora baseline

--- model/baselines.py
+++ model/baselines.py
@@ -8,6 +8,11 @@ from sys import exit
 import numpy as np
 from scipy.sparse import coo_matrix, hstack
 
+import os
+from os.path import join as pjoin
+import pickle
+import sklearn.decomposition
+
 parser = argparse.ArgumentParser(description='Baselines')
 
 parser.add_argument("--corpus", type=str, default='books_5', help="books_5|books_old_5|books_8|books_all|gw_5|gw_8")
@@ -20,6 +25,84 @@ parser.add_argument("--concat_sentences", type=bool, default=False)
 
 params, _ = parser.parse_known_args()
 print(params)
+
+_PAD = "<pad>" # no need to pad
+_UNK = "<unk>"
+_START_VOCAB = [_PAD, _UNK]
+
+def process_glove(glove_file, vocab_dict, save_path, random_init=True):
+    """
+    :param vocab_list: [vocab]
+    :return:
+    """
+    if os.path.isfile(save_path + ".pkl"):
+        print("Glove file already exists at %s" % (save_path + ".pkl"))
+    else:
+        glove_path = os.path.join(glove_file)
+        if random_init:
+            glove = {v: np.random.randn(300) for v in vocab_dict}
+        else:
+            glove = {v: np.zeros(300) for v in vocab_dict}
+
+        found = 0
+
+        for line in open(glove_path, "r"):
+            word, vec = line.split(" ", 1)
+            if word in vocab_dict:
+                glove[word] = np.fromstring(vec, sep=" ")
+                found += 1
+
+        pickle.dump(glove, open(save_path + ".pkl", "wb"))
+
+        print("saved glove data to: {}".format(save_path))
+    return pickle.load(open(save_path + ".pkl", "rb"))
+
+def create_vocabulary(vocabulary_path, corpus, discourse_markers=None):
+	if os.path.isfile(vocabulary_path):
+		print("Vocabulary file already exists at %s" % vocabulary_path)
+	else:
+		print("Creating vocabulary {}".format(vocabulary_path))
+
+	vocab = {s:0 for s in _START_VOCAB}
+	counter = 0
+
+	for split in ["train", "test", "valid"]:
+		split_data = corpus[split]
+		for s_tag in ["s1", "s2"]:
+			s_data = split_data[s_tag]
+			for s in s_data:
+				counter+=1
+				if counter % 100000 == 0:
+					print("processing line %d" % counter)
+				for w in s.split(" "):
+					if not w in _START_VOCAB:
+						if w in vocab:
+							vocab[w] += 1
+						else:
+							vocab[w] = 1
+
+	vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+	print("Vocabulary size: %d" % len(vocab_list))
+	with open(vocabulary_path, mode="w") as vocab_file:
+		for w in vocab_list:
+			vocab_file.write("{}\t{}\n".format(w, vocab[w]))
+
+def initialize_vocabulary(vocabulary_path):
+	# map vocab to word embeddings
+	if os.path.isfile(vocabulary_path):
+		counts = {}
+		total = 0
+		rev_vocab = []
+		for line in open(vocabulary_path, mode="r"):
+			s, c = line[:-1].split('\t')
+			c = int(c)
+			counts[s] = c
+			total += c
+			rev_vocab.append(s)
+		vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
+		return vocab, rev_vocab, counts, total
+	else:
+		raise ValueError("Vocabulary file %s not found.", vocabulary_path)
 
 
 def unigrams_phi(words):
@@ -210,9 +293,86 @@ def run_baseline_BoW_model(params):
 	np.save("predicted.txt", test_pred)
 	np.save("actual.txt", test_y)
 
+def get_X(lst, glove_dict, counts, total, a):
+	X = []
+	for s in lst:
+		words = s.split(" ")
+		weighted_word_embeddings = []
+		for word in words:
+			vw = glove_dict[word]
+			pw = counts[word]/total
+			weighted_word_embeddings.append(a/(a+pw)*vw)
+		vs = 1.0/len(words) * np.sum(weighted_word_embeddings, axis=0)
+		X.append(vs)
+	return np.matrix(X)
+
 def run_baseline_arora_model(params):
+	glove_file = "/home/anie/glove/glove.840B.300d.txt"
+	vocab_path = pjoin(params.outputdir, params.corpus + "_vocab.dat")
 	corpus = read_corpus(params.corpus)
+	if params.run_through_subset:
+		corpus["train"]["s1"] = corpus["train"]["s1"][:100]
+		corpus["train"]["s2"] = corpus["train"]["s2"][:100]
+		corpus["train"]["label"] = corpus["train"]["label"][:100]
+		corpus["test"]["s1"] = corpus["test"]["s1"][:100]
+		corpus["test"]["s2"] = corpus["test"]["s2"][:100]
+		corpus["test"]["label"] = corpus["test"]["label"][:100]
+
+	# create_vocabulary(vocab_path, corpus)
+	vocab, rev_vocab, counts, total = initialize_vocabulary(vocab_path)
 	# get word vectors
+	glove_dict = process_glove(glove_file, vocab, pjoin(params.outputdir, params.corpus + "_glove"))
+	a = 0.001
+
+	print("processing corpus")
+	X_train1 = get_X(corpus["train"]["s1"], glove_dict, counts, total, a)
+	X_train2 = get_X(corpus["train"]["s2"], glove_dict, counts, total, a)
+	X_valid1 = get_X(corpus["valid"]["s1"], glove_dict, counts, total, a) 
+	X_valid2 = get_X(corpus["valid"]["s2"], glove_dict, counts, total, a)
+	X_test1 = get_X(corpus["test"]["s1"], glove_dict, counts, total, a) 
+	X_test2 = get_X(corpus["test"]["s2"], glove_dict, counts, total, a)
+	X = np.concatenate((X_train1, X_train2, X_valid1, X_valid2, X_test1, X_test2), axis=0)
+	pca = sklearn.decomposition.PCA(n_components=1)
+	pca.fit(X)
+	u = pca.components_
+	projection_matrix = np.matmul(np.transpose(u), u)
+
+	print("getting train and test sets")
+	X_train1 = X_train1 - np.transpose(np.matmul(projection_matrix, np.transpose(X_train1)))
+	X_train2 = X_train2 - np.transpose(np.matmul(projection_matrix, np.transpose(X_train2)))
+	train_X = np.concatenate((X_train1, X_train2), axis=1)
+	train_y = corpus["train"]["label"]
+
+	X_test1 = X_test1 - np.transpose(np.matmul(projection_matrix, np.transpose(X_test1)))
+	X_test2 = X_test2 - np.transpose(np.matmul(projection_matrix, np.transpose(X_test2)))
+	test_X = np.concatenate((X_test1, X_test2), axis=1)
+	test_y = corpus["test"]["label"]
+
+	# get 1st PC of X_train --> u
+	# rewrite v_s
+	# run linear regression
+	print("fitting model")
+	model = sklearn.linear_model.LogisticRegression(max_iter=100, verbose=0, fit_intercept=True)
+	model.fit(train_X, train_y)
+	print("testing model")
+	test_pred = model.predict(test_X)
+	results = {label: {"hits": 0, "actual": 0.0000001, "predicted": 0.00000001} for label in set(train_y)}
+	for i in range(len(test_y)):
+		actual = test_y[i]
+		predicted = test_pred[i]
+		if actual==predicted:
+			results[actual]["hits"] += 1
+		results[actual]["actual"] += 1
+		results[predicted]["predicted"] += 1
+	for label in results:
+		precision = 100*float(results[label]["hits"]) / results[label]["predicted"]
+		recall = 100*float(results[label]["hits"]) / results[label]["actual"]
+		print("{}: precision={:.2f}, recall={:.2f}".format(label, precision, recall))
+	output = model.score(test_X, test_y)
+	print("accuracy: {}".format(output))
+	print("accuracy: {}".format(np.sum(test_pred==test_y)/len(test_pred)))
+	np.save("predicted.txt", test_pred)
+	np.save("actual.txt", test_y)
 
 if __name__ == '__main__':
 	if params.features == "mostcommonclass":
